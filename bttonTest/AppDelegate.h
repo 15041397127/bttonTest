7f8447ac42f81e7feb2127476bd710541eb55a36
ZhangXu <zhangxu@zxdemacbook-pro.local>
2016-07-06 10:05:21 +0800

    Initial Commit

---
+++ bttonTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bttonTest
+//
+//  Created by 张旭 on 16/7/6.
+//  Copyright © 2016年 zhangXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bttonTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bttonTest
+//
+//  Created by 张旭 on 16/7/6.
+//  Copyright © 2016年 zhangXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
